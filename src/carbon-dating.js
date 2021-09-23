498c359acb09bcb37a3fdf80beaf3ac899631d35
juneagain <juneagain@mail.ru>
2021-09-23 16:22:52 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sa) {
+  if (typeof sa == "string" && parseFloat(sa) > 0 && parseFloat(sa) < 15.1) {
+    let age = MODERN_ACTIVITY / sa; // ?
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.log(age) / k;
+    return Math.ceil(t);
+    } else {
+      return false;
+    }
 }
